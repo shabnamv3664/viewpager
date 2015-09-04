36063db8f5328499ac11f08e2f817ff03195aa76
shabnamv3664 <shabnam@wiztelsys.com>
2015-09-04 17:26:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/administrator/viewpager/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.administrator.viewpager;
+
+import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+
+
+    }
+
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int pos) {
+            switch (pos) {
+
+                case 0:
+                    return FirstFragment.newInstance("FirstFragment");
+                case 1:
+                    return SecondFragment.newInstance("SecondFragment");
+
+                default:
+                    return SecondFragment.newInstance("Default");
+
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+
+
+        }
+    }
+}
